a8fdf482d0a0adc73aefc35a7e8b754d65d106bb
LoaStaub <35306946+LoaStaub@users.noreply.github.com>
2023-06-03 02:37:29 +0200

    Pushing last functions

--- Unmarker/Frame.lua
+++ Unmarker/Frame.lua
@@ -1,15 +1,14 @@
--- Author      : Taurentius
+-- Author      : LoaStaub
 -- Create Date : 01.06.2023 21:46:03
 
 function ButtonAccept_OnClick()
-	print("Taurentius: Save")
 	UnmarkerDB.OpenWorld = CheckButtonOpenWorld:GetChecked()
 	UnmarkerDB.PVP = CheckButtonPVP:GetChecked()
 	UnmarkerDB.Raids = CheckButtonRaids:GetChecked()
 	UnmarkerDB.Kick = CheckButtonKick:GetChecked()
 	UnmarkerDB.Times = EditBoxTimes:GetText()
 	UnmarkerDB.Dungeons = CheckButtonDungeons:GetChecked()
-	print("Taurentius: Save success")
+	Frame1:Hide()
 end
 
 function ButtonAbort_OnClick()
@@ -17,14 +16,10 @@ function ButtonAbort_OnClick()
 end
 
 function Frame1_OnShow()
-	print("OnShow Step 1")
 	if not UnmarkerDB then
-		print("OnShow INIT DB")
 		UnmarkerDB = {}
-		UnmarkerDB.Times = "3"
 		Frame1:Hide()
 	end
-	print("OnShow Step 2")
 	CheckButtonPVP:SetChecked(UnmarkerDB.PVP)
 	CheckButtonDungeons:SetChecked(UnmarkerDB.Dungeons)
 	CheckButtonOpenWorld:SetChecked(UnmarkerDB.OpenWorld)
--- Unmarker/Main.lua
+++ Unmarker/Main.lua
@@ -1,43 +1,65 @@
--- Author      : Taurentius
+-- Author      : LoaStaub
 -- Create Date : 01.06.2023 22:52:42
+
+-- /dump select(4, GetBuildInfo())
 
 SLASH_UNMARKER1 = "/unmarker"
 
 SlashCmdList["UNMARKER"] = function(msg)
-    print("Funktioniert")
 	Frame1:Show()
 end
 
 local frame = CreateFrame("Frame")
 local ownGuid = UnitGUID("player")
+local warnCounter = 0
 
 function frame:CHAT_MSG_TARGETICONS(text, playerName, languageName, channelName, playerName2, specialFlags, zoneChannelID, channelIndex, channelBaseName, languageID, lineID, guid, bnSenderID, isMobile, isSubtitle, hideSenderInLetterbox, supressRaidIcons)
-	print("MARKED")
-	print("text")
-	print(text) -- [ ist 1
 	locClass, engClass, locRace, engRace, gender, name, server = GetPlayerInfoByGUID(ownGuid)
-	print("name")
-	print(name)
 	if string.find(text, name)
 	then
-		print("FUNKTIONIERT")
 		local index = 0
 		local evildoer
 		for w in string.gmatch(text, "%a+") do
 			index = index + 1
 			if index == 2 then
 				evildoer = w
-				print(evildoer)
 			end
 		end
-		-- SendChatMessage(text ,"PARTY")
-		SendChatMessage("STOP IT NOW" ,"SAY")
-		SetRaidTarget("player", 0)
+		inInstance, instanceType = IsInInstance()
+		if instanceType == "pvp" and UnmarkerDB.PVP == true then
+			WarnThem(evildoer)
+		elseif instanceType == "arena" and UnmarkerDB.PVP == true then
+			WarnThem(evildoer)
+		elseif instanceType == "party" and UnmarkerDB.Dungeons == true then
+			WarnThem(evildoer)
+		elseif instanceType == "raid" and UnmarkerDB.Raids == true then
+			WarnThem(evildoer)
+		elseif instanceType == "none" and UnmarkerDB.OpenWorld == true then
+			WarnThem(evildoer)
+		end		
 	end
+end
+
+function WarnThem(evildoer)
+	SendChatMessage(evildoer.." STOP CASTING ICONS ON ME! NOW!" ,"PARTY")
+	SetRaidTarget("player", 0)
+	if UnmarkerDB.Kick == true then
+		warnCounter = warnCounter + 1
+		if tostring(warnCounter) == UnmarkerDB.Times then
+			warnCounter = 0
+			SendChatMessage(evildoer.." I WILL KICK YOU NOW!" ,"PARTY")
+		end
+	end
+end
+
+function frame:ZONE_CHANGED_NEW_AREA()
+	warnCounter = 0
 end
 
 frame:SetScript("OnEvent", function(self, event, ...)
 	self[event](self, ...)
 end)
 
+
 frame:RegisterEvent("CHAT_MSG_TARGETICONS")
+frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
